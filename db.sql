db97c652bb13e27499c3a656dc217f212bf93c17
Juhosk <n0kaju00@students.oamk.fi>
2022-03-27 12:56:53 +0300

    Categoryyn lisätty arvojen lisäys lausekkeet

--- db.sql
+++ db.sql
@@ -6,3 +6,12 @@ create table category (
     id int primary key auto_increment,
     name varchar(255) not null
 );
+
+/* 
+
+insert into category (name) values ('Tietokoneet');
+insert into category (name) values ('Komponentit');
+insert into category (name) values ('Oheislaitteet');
+
+
+*/
