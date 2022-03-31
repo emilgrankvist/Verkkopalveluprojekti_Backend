a37490c29db0c84d88d8dd864e8a11eca22c0e3a
Aktonen <aku.tolonen1999@hotmail.fi>
2022-03-31 14:09:20 +0300

    Tuote taulun luomislauseet sekä testi arvot

--- db.sql
+++ db.sql
@@ -7,11 +7,26 @@ create table category (
     name varchar(255) not null
 );
 
+create table product (
+    id int PRIMARY key AUTO_INCREMENT,
+    name varchar(100) not null,
+    price double (10,2) not null,
+    image varchar(50),
+    category_id int not null,
+    index category_id(category_id),
+    FOREIGN KEY (category_id) REFERENCES category(id)
+    on delete restrict
+);
+
 /* 
+
+TESTIARVOJA
 
 insert into category (name) values ('Tietokoneet');
 insert into category (name) values ('Komponentit');
 insert into category (name) values ('Oheislaitteet');
 
+insert into product (name, price, category_id) values ('Test computer 1',10,1);
+insert into product (name, price, category_id) values ('Test computer 2',20,1);
 
 */
