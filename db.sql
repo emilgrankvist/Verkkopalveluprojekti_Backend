e3a9817fb13e2df05443782c4d4e8cea55b9f620
Emil Grankvist <emil.grankvist@icloud.com>
2022-04-20 12:25:55 +0300

    luontilauseet oli väärässä järjestyksessä, ei voi luoda order row ennen productia jos se viittaa siihen

--- db.sql
+++ db.sql
@@ -1,96 +1,98 @@
-/*tietokannan luonti*/
-drop database if exists webshop;
-create database webshop;
-use webshop;
-/*asiakas taulun luonti*/
-drop table if exists customer;
-create table customer (
-id int primary key AUTO_INCREMENT,
-firstname varchar(100) not null,
-lastname varchar(100) not null,
-address varchar(100) not null,
-zip varchar(20) not null,
-city varchar(50) not null
-);
-/*tilaus taulun luonti*/
-drop table if exists `order`;
-
-create table `order` (
+    /*tietokannan luonti*/
+    drop database if exists webshop;
+    create database webshop;
+    use webshop;
+    /*asiakas taulun luonti*/
+    drop table if exists customer;
+    create table customer (
     id int primary key AUTO_INCREMENT,
-    order_date timestamp default CURRENT_TIMESTAMP,
-    customer_id int not null,
-    index customer_id(customer_id),
-    foreign key (customer_id) references customer(id)
-    on delete restrict
-)
+    firstname varchar(100) not null,
+    lastname varchar(100) not null,
+    address varchar(100) not null,
+    zip varchar(20) not null,
+    city varchar(50) not null
+    );
+    /*tilaus taulun luonti*/
+    drop table if exists `order`;
 
-/*tilausrivi taulun luonti*/
-drop table if exists order_row;
+    create table `order` (
+        id int primary key AUTO_INCREMENT,
+        order_date timestamp default CURRENT_TIMESTAMP,
+        customer_id int not null,
+        index customer_id(customer_id),
+        foreign key (customer_id) references customer(id)
+        on delete restrict
+    );
 
-create table order_row(
-order_id int not null,
-index order_id(order_id),
-foreign key (order_id) REFERENCES `order`(id)
-on delete restrict,
-product_id int not null,
-index product_id(product_id),
-foreign key (product_id) references product(id)
-on delete RESTRICT,
-amount INTEGER
-)
 
-/*Kategoria taulun luonti */
-drop table if exists category;
-create table category (
-    id int primary key auto_increment,
-    name varchar(255) not null
-);
 
-/*Arvoja kategoria tauluun*/
-insert into category (name) values ('Tietokoneet');
-insert into category (name) values ('Komponentit');
-insert into category (name) values ('Oheislaitteet');
-insert into category (name) values ('Konsolit');
-/*Tuote taulun luonti*/
-drop table if exists product;
-create table product (
-    id int PRIMARY key AUTO_INCREMENT,
-    name varchar(100) not null,
-    price double (10,2) not null,
-    image varchar(50),
-    category_id int not null,
-    index category_id(category_id),
-    FOREIGN KEY (category_id) REFERENCES category(id)
-    on delete restrict
-);
+    /*Kategoria taulun luonti */
+    drop table if exists category;
+    create table category (
+        id int primary key auto_increment,
+        name varchar(255) not null
+    );
 
-/*komponentteja tauluun */
-insert into product (name, price, image, category_id) values ('Nviadia GeForce RTX 2080Ti',999,'RTX 2080TI.png',2);
-insert into product (name, price, image, category_id) values ('Nviadia GeForce RTX 2080',699,'RTX 2080.jpg',2);
-insert into product (name, price, image, category_id) values ('Nviadia GeForce GTX 1080Ti',699,'GTX 1080TI.jpg',2);
-insert into product (name, price, image, category_id) values ('Nvidia GeForce RTX 2080 AORUS',1500,'aorus gtx 3080.jpg',2);
-insert into product (name, price, image, category_id) values ('AMD Ryzen 5 3600',240,'AMD Ryzen 5 3600.jpg',2);
-insert into product (name, price, image, category_id) values ('Intel i5-9400F',150,'INTEL i5-9400F.jpg',2);
+    /*Arvoja kategoria tauluun*/
+    insert into category (name) values ('Tietokoneet');
+    insert into category (name) values ('Komponentit');
+    insert into category (name) values ('Oheislaitteet');
+    insert into category (name) values ('Konsolit');
 
-/*oheislaitteita tauluun */
-insert into product (name, price, image, category_id) values ('Playstation 5 ohjain',70,'ps5 ohjain.jpg',3);
-insert into product (name, price, image, category_id) values ('Playstation 4 ohjain',55,'PS4 Ohjain.jpg',3);
-insert into product (name, price, image, category_id) values ('Xbox Series X/One ohjain',63,'xbox one ohjain.jpg',3);
+    /*Tuote taulun luonti*/
+    drop table if exists product;
+    create table product (
+        id int PRIMARY key AUTO_INCREMENT,
+        name varchar(100) not null,
+        price double (10,2) not null,
+        image varchar(50),
+        category_id int not null,
+        index category_id(category_id),
+        FOREIGN KEY (category_id) REFERENCES category(id)
+        on delete restrict
+    );
 
-/*Konsoleita tauluun */
-insert into product (name, price, image, category_id) values ('Sony Playstation 5 + ohjain',530,'PS5 + ohjain.jpg',4);
-insert into product (name, price, image, category_id) values ('Sony Playstation 4 + ohjain',330,'ps4.jpg',4);
-insert into product (name, price, image, category_id) values ('Microsoft Xbox One + ohjain',330,'xbox ONE + ohjain.jpg',4);
-insert into product (name, price, image, category_id) values ('Microsoft Xbox Series X + ohjain',510,'xbox series X.jpg',4);
-/* 
+    /*tilausrivi taulun luonti*/
+    drop table if exists order_row;
 
-TESTIARVOJA
+    create table order_row(
+    order_id int not null,
+    index order_id(order_id),
+    foreign key (order_id) REFERENCES `order`(id),
+    product_id int not null,
+    index product_id(product_id),
+    foreign key (product_id) references product(id),
+    amount INTEGER
+    );
 
-insert into category (name) values ('Tietokoneet');
-insert into category (name) values ('Komponentit');
-insert into category (name) values ('Oheislaitteet');
 
-insert into product (name, price, category_id) values ('Test computer 1',10,1);
-insert into product (name, price, category_id) values ('Test computer 2',20,1);
+    /*komponentteja tauluun */
+    insert into product (name, price, image, category_id) values ('Nviadia GeForce RTX 2080Ti',999,'RTX 2080TI.png',2);
+    insert into product (name, price, image, category_id) values ('Nviadia GeForce RTX 2080',699,'RTX 2080.jpg',2);
+    insert into product (name, price, image, category_id) values ('Nviadia GeForce GTX 1080Ti',699,'GTX 1080TI.jpg',2);
+    insert into product (name, price, image, category_id) values ('Nvidia GeForce RTX 2080 AORUS',1500,'aorus gtx 3080.jpg',2);
+    insert into product (name, price, image, category_id) values ('AMD Ryzen 5 3600',240,'AMD Ryzen 5 3600.jpg',2);
+    insert into product (name, price, image, category_id) values ('Intel i5-9400F',150,'INTEL i5-9400F.jpg',2);
 
-*/
+    /*oheislaitteita tauluun */
+    insert into product (name, price, image, category_id) values ('Playstation 5 ohjain',70,'ps5 ohjain.jpg',3);
+    insert into product (name, price, image, category_id) values ('Playstation 4 ohjain',55,'PS4 Ohjain.jpg',3);
+    insert into product (name, price, image, category_id) values ('Xbox Series X/One ohjain',63,'xbox one ohjain.jpg',3);
+
+    /*Konsoleita tauluun */
+    insert into product (name, price, image, category_id) values ('Sony Playstation 5 + ohjain',530,'PS5 + ohjain.jpg',4);
+    insert into product (name, price, image, category_id) values ('Sony Playstation 4 + ohjain',330,'ps4.jpg',4);
+    insert into product (name, price, image, category_id) values ('Microsoft Xbox One + ohjain',330,'xbox ONE + ohjain.jpg',4);
+    insert into product (name, price, image, category_id) values ('Microsoft Xbox Series X + ohjain',510,'xbox series X.jpg',4);
+    /* 
+
+    TESTIARVOJA
+
+    insert into category (name) values ('Tietokoneet');
+    insert into category (name) values ('Komponentit');
+    insert into category (name) values ('Oheislaitteet');
+
+    insert into product (name, price, category_id) values ('Test computer 1',10,1);
+    insert into product (name, price, category_id) values ('Test computer 2',20,1);
+
+    */
